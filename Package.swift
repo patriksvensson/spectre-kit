b37c3a6b63e70ae8d8747dff37a5d69944d9078f
Patrik Svensson <patrik@patriksvensson.se>
2023-12-03 18:21:21 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SpectreKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SpectreKit",
+            targets: ["SpectreKit"]),
+        .executable(
+            name: "Example",
+            targets: ["Example"])
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SpectreKit"),
+        .testTarget(
+            name: "SpectreKitTests",
+            dependencies: ["SpectreKit"]),
+        .executableTarget(
+            name: "Example",
+            dependencies: ["SpectreKit"])
+    ]
+)
